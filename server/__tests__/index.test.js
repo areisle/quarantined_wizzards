2a2d0e59a600125c89fe642c914c9cf97f2de53c
creisle <caralynreisle@gmail.com>
2020-04-12 21:19:23 -0700

    add tests for play-card

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -2,6 +2,12 @@ const io = require('socket.io-client');
 const getPort = require('get-port');
 
 const { server: createServer, close: closeServer } = require('../');
+const db = require('../db');
+
+Error.stackTraceLimit = Infinity;
+
+
+const SUITS = ['wizard', 'jester', 'hearts', 'spades', 'diamonds', 'clubs'];
 
 let port,
     clientSocket;
@@ -13,33 +19,39 @@ beforeAll(async () => {
     await createServer({ port });
 });
 
-test('create-game', (done) => {
-    clientSocket.on('new-game', (gameId) => {
-        expect(typeof gameId).toBe('string');
-        done();
-    });
-    clientSocket.emit('create-game');
+afterAll(async () => {
+    closeServer();
+    await clientSocket.close();
 });
+
+describe('create-game', () => {
+    test('callback', (done) => {
+        clientSocket.emit('create-game', (gameId) => {
+            expect(typeof gameId).toBe('string');
+            done();
+        });
+    });
+});
+
 
 describe('join-game', () => {
     let gameId;
 
     beforeAll((done) => {
-        clientSocket.on('new-game', (gameIdIn) => {
+        clientSocket.emit('create-game', (gameIdIn) => {
             gameId = gameIdIn;
             done();
         });
-        clientSocket.emit('create-game');
     });
 
-    test('client gets user-joined event', (done) => {
-        clientSocket.on('user-joined', (playerId) => {
+    test('callback', (done) => {
+        clientSocket.emit('join-game', gameId, 'blargh', (playerId) => {
+            expect([0, 1]).toContain(playerId);
             done();
         });
-        clientSocket.emit('join-game', gameId, 'blargh');
     });
 
-    test('client gets users-changed event', (done) => {
+    test('users-changed', (done) => {
         clientSocket.on('users-changed', (players) => {
             expect(players.length).toBeGreaterThanOrEqual(1);
             expect(players[0]).toHaveProperty('playerId', 0);
@@ -47,9 +59,129 @@ describe('join-game', () => {
         });
         clientSocket.emit('join-game', gameId, 'monkeys');
     });
-})
+});
 
-afterAll(async () => {
-    closeServer();
-    await clientSocket.close();
+describe('start-game', () => {
+    let gameId,
+        players;
+
+    beforeEach(async () => {
+        gameId = await new Promise((resolve) => {
+            clientSocket.emit('create-game', resolve);
+        });
+        players = await Promise.all(['blargh', 'monkeys', 'fishmonger'].map(name => {
+            return new Promise((resolve) => {
+                clientSocket.emit('join-game', gameId, name, resolve);
+            });
+        }));
+    });
+
+    afterEach(async () => {
+        await db.deleteGame(gameId);
+    });
+
+    test('trump-changed', (done) => {
+        clientSocket.on('trump-changed', (suit) => {
+            expect(SUITS).toContain(suit);
+            done();
+        });
+        clientSocket.emit('start-game', gameId);
+    });
+
+    test('active-user-changed', (done) => {
+        clientSocket.on('active-user-changed', (playerId) => {
+            expect(playerId).toBeLessThanOrEqual(2);
+            expect(playerId).toBeGreaterThanOrEqual(0);
+            done();
+        });
+        clientSocket.emit('start-game', gameId);
+    });
+
+    test('cards-dealt', (done) => {
+        clientSocket.on('cards-dealt', (cards) => {
+            cards.forEach((card) => {
+                expect(card).toHaveProperty('number');
+                expect(card).toHaveProperty('suit');
+                expect(SUITS).toContain(card.suit);
+            });
+            expect(cards).toHaveProperty('length', 1);
+            done();
+        });
+        clientSocket.emit('start-game', gameId);
+    });
+});
+
+describe('play-card', () => {
+    let gameId,
+        players;
+
+    beforeEach(async () => {
+        gameId = await new Promise((resolve) => {
+            clientSocket.emit('create-game', resolve);
+        });
+        players = await Promise.all(['blargh', 'monkeys', 'fishmonger'].map(name => {
+            return new Promise((resolve) => {
+                clientSocket.emit('join-game', gameId, name, resolve);
+            });
+        }));
+        await new Promise((resolve) => {
+            clientSocket.emit('start-game', gameId, resolve);
+        });
+    });
+
+    afterEach(async () => {
+        await db.deleteGame(gameId);
+    });
+
+    test('lead-changed', (done) => {
+        clientSocket.on('lead-changed', (suit) => {
+            expect(SUITS).toContain(suit);
+            done();
+        });
+        clientSocket.emit('play-card', gameId, players[players.length - 1], 'clubs', 1);
+    });
+
+    test('card-played', (done) => {
+        clientSocket.on('card-played', (resp) => {
+            expect(resp).toEqual({ card: { suit: 'clubs', number: 1 }, playerId: players[players.length - 1] })
+            done();
+        });
+        clientSocket.emit('play-card', gameId, players[players.length - 1], 'clubs', 1);
+    });
+
+    test('card-played (error)', (done) => {
+        clientSocket.on('error', (msg) => {
+            expect(msg).toContain('Invalid play: It is not this users (0) turn. Waiting for player 2 to complete their turn')
+            done();
+        });
+        clientSocket.emit('play-card', gameId, players[0], 'clubs', 1);
+    });
+
+    describe('trick-won', () => {
+        beforeEach(async () => {
+            await (new Promise(resolve => clientSocket.emit(
+                'play-card',
+                gameId,
+                2,
+                'clubs',
+                1,
+                resolve
+            )));
+            await (new Promise(resolve => clientSocket.emit(
+                'play-card',
+                gameId,
+                0,
+                'clubs',
+                2,
+                resolve
+            )));
+        });
+
+        test('high number', (done) => {
+            clientSocket.on('trick-won', (resp) => {
+                done();
+            });
+            clientSocket.emit('play-card', gameId, 1, 'clubs', 3);
+        });
+    });
 });
