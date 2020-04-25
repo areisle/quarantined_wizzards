8496ff86c2f788017897f4d251941eb65b71445c
creisle <caralynreisle@gmail.com>
2020-04-25 13:14:00 -0700

    feat: add join game option to start game dialog
    resovles: #115

--- src/components/StartGameDialog/index.tsx
+++ src/components/StartGameDialog/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { 
-    Button, 
-    Dialog, 
+import './index.scss';
+
+import React, { useState, useCallback } from 'react';
+import {
+    Button,
+    Dialog,
     DialogActions,
     DialogTitle,
+    TextField,
+    TextFieldProps,
+    DialogContent,
 } from '@material-ui/core';
 
 interface StartGameDialogProps {
     open: boolean;
-    onStart: () => void;
+    onStart: (newGameId: string) => void;
 }
 
 function StartGameDialog(props: StartGameDialogProps) {
@@ -17,20 +22,49 @@ function StartGameDialog(props: StartGameDialogProps) {
         onStart,
     } = props;
 
+    const [gameId, setGameId] = useState('');
+
+    const handleGameIdChange: TextFieldProps['onChange'] = (event) => {
+        setGameId(event?.target?.value);
+    };
+
+    const handleJoinGame = useCallback(() => {
+        onStart(gameId);
+    }, [onStart, gameId]);
+
+    const [isJoining, setIsJoining] = useState(false);
+
     return (
         <Dialog 
             open={open}
+            className="start-join-game"
         >
             <DialogTitle>
                 Welcome to Quarantined Wizzards!
             </DialogTitle>
-            <DialogActions>
+            <DialogContent>
+                {isJoining && (<TextField
+                    onChange={handleGameIdChange}
+                    label="enter a game ID"
+                    fullWidth
+                />)}
+            </DialogContent>
+            <DialogActions className="start-join-game__actions">
+                {!isJoining && (
+                    <Button onClick={() => setIsJoining(true)}>join existing game</Button>
+                )}
+                {isJoining && (
+                    <Button onClick={() => setIsJoining(false)}>back</Button>
+                )}
                 <Button
-                    onClick={onStart}
+                    onClick={handleJoinGame}
                     color='primary'
                     variant='contained'
                 >
-                    start a new game
+                    {isJoining
+                        ? "join game"
+                        : "start a new game"
+                    }
                 </Button>
             </DialogActions>
         </Dialog>
