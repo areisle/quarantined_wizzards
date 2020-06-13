2d643ef0171f2d88bff9d54e4e7b3ec08657dadc
areisle <areisle@gmail.com>
2020-06-13 12:34:37 -0700

    fix: add missing react import for hook

--- src/utilities.tsx
+++ src/utilities.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
 import { useEffect, useRef } from "react";
 import { GameState, PlayerId, ScoreWithTotal } from "./types";
 
